fec9e625efb66d0479b5b61fab15ba45a9d02d61
Jesse Lindström <jesselindstrom@hotmail.com>
2025-03-01 14:33:52 +0200

    Update etusivu.js

--- etusivu.js
+++ etusivu.js
@@ -1,34 +1,34 @@
 {
-    "otsikko"; "JSON-datan käsittelyä",
-        "kuvaus"; "Tällä sivustolla haetaan JSON-muotoista dataa",
-            "kuva"; "https://jaakkola.github.io/json/digi.JPG",
-                "opintojakso"; {
-        "nimi"; "Digitekniikat",
-            "tunnus"; "DIG4TA021",
-                "opintopisteet"; 5,
-                    "sisalto";[
-                        "Low-code",
-                        "Sisällönhallintajärjestelmät",
-                        "Responsiivinen käyttöliittymä ja komponentit",
-                        "Tiedon haku julkisista palveluista REST-rajapinnan yli"
-                    ],
-                        "tekniikat";[
-                            {
-                                "aihe": "Low-code",
-                                "linkki": "https://www.weebly.com/"
-                            },
-                            {
-                                "aihe": "WordPress",
-                                "linkki": "https://wordpress.com/"
-                            },
-                            {
-                                "aihe": "Bootstrap",
-                                "linkki": "https://getbootstrap.com/"
-                            },
-                            {
-                                "aihe": "JSON",
-                                "linkki": "https://www.json.org/json-en.html"
-                            }
-                        ]
+    "otsikko": "JSON-datan käsittelyä",
+    "kuvaus": "Tällä sivustolla haetaan JSON-muotoista dataa",
+    "kuva": "https://jaakkola.github.io/json/digi.JPG",
+    "opintojakso": {
+        "nimi": "Digitekniikat",
+        "tunnus": "DIG4TA021",
+        "opintopisteet": 5,
+        "sisalto": [
+            "low-code",
+            "sisällönhallintajärjestelmät",
+            "responsiivinen käyttöliittymä ja komponentit",
+            "tiedon haku julkisista palveluista REST-rajapinnan yli"
+        ],
+        "tekniikat": [
+            {
+                "aihe": "low-code",
+                "linkki": "https://www.weebly.com/"
+            },
+            {
+                "aihe": "WordPress",
+                "linkki": "https://wordpress.com/"
+            },
+            {
+                "aihe": "Bootstrap",
+                "linkki": "https://getbootstrap.com/"
+            },
+            {
+                "aihe": "JSON",
+                "linkki": "https://www.json.org/json-en.html"
+            }
+        ]
     }
 }
